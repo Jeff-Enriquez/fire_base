bb9a7a731b9ea1cb667eadd10dcfd18f3ecf37f1
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-01-18 23:36:15 -0800

    Render comments All comments on a video will be rendered to the view

--- controllers/videos.js
+++ controllers/videos.js
@@ -1,9 +1,16 @@
 const Comment = require('../models/comment');
 
 const show = (req, res) => {
-  res.render('videos/show', {
-    user: req.user,
-    id: req.params.id
+  let id = req.params.id;
+  Comment.find({video: id}).populate('user').exec((err, comments) => {
+    if(err) return res.redirect(`/videos/${id}`);
+    console.log(comments);
+    console.log(comments[0].user.avatar);
+    res.render('videos/show', {
+      user: req.user,
+      comments,
+      id,
+    });
   });
 }
 const newComment = (req, res) => {
