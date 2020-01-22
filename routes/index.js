6e2f40f1b9070edd40ba0384fc30b8d98b8e8960
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-01-22 14:49:03 -0800

    Refactor server file

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,13 @@ router.get('/auth/google', passport.authenticate(
   { scope: ['profile', 'email'] }
 ));
 // Google OAuth callback route
-router.get('/oauth2callback', test, passport.authenticate(
+router.get('/oauth2callback', passport.authenticate(
   'google',
   {
     successRedirect : '/',
     failureRedirect : '/'
   }
 ));
-
-function test (req, res, next) {
-  console.log('hit')
-  return next()
-}
 
  // OAuth logout route
  router.get('/logout', function(req, res){
