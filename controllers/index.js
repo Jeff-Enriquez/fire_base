aeffabfbfe01e9832afdc8a1a3b5e5962c690f23
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-01-21 17:11:21 -0800

    Shorten the video titles on index page

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,11 @@ const Video = require('../models/video');
 
 const show = (req, res) => {
   Video.find({}, (err, videos) => {
+    for(let i = 2; i < videos.length; i++){
+      if(videos[i].title.length > 27){
+        videos[i].title = `${videos[i].title.slice(0,27)}...`;
+      }
+    }
     if(err) {
       res.render('index', {
         user: req.user,
